9f75c79e892732da55624f0f453c4657d83ee358
Marco Binda <marcobinda@yahoo.com>
2023-10-12 21:23:40 -0300

    adding routes

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import figlet from "figlet"
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(req){
+        const url = new URL(req.url)
+        
+        if(url.pathname === "/"){
+            const body = figlet.textSync("video!")
+            return new Response(body)
+        }
+
+        if(url.pathname === "/about"){
+            return new Response("About me!")
+        }
+
+        if(url.pathname === "/contact"){
+            return new Response("Contact us!")
+        }
+
+        return new Response("404!")
+    }
+})
+
+
+
+console.log(`Listenin on PORT http:localhost:${server.port}`)
